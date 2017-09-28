32136df4224470333a6695e07e29e3042782904b
horaciocoronel <arielc@gmail.com>
2017-09-28 16:42:04 -0400

    Finish bonus 1 from exercise 3

--- solar_system.rb
+++ solar_system.rb
@@ -1,4 +1,5 @@
-# require 'pp'
+require 'pry'
+
 class System
 
   def initialize
@@ -6,7 +7,15 @@ class System
   end
 
   def add(celestial_body)
-    @bodies << celestial_body
+    # is_duplicated(celestial_body)
+# binding.pry
+      if is_duplicated(celestial_body) == true
+        puts "It already exists."
+# binding.pry
+      else
+        @bodies << celestial_body
+          # binding.pry
+      end
   end
 
   def bodies
@@ -15,6 +24,20 @@ class System
 
   def total_mass
     @bodies[mass].sum
+  end
+
+  def is_duplicated(celestial_body)
+    @bodies.each do |body|
+      # binding.pry
+      if body.name == celestial_body.name && body.class == celestial_body.class
+        return true
+      else
+      end
+    end
+    return false
+    #do .each
+    # and filter by name and class
+    # .class
   end
 end
 
@@ -39,6 +62,9 @@ class Planet < Body
     @day = day
     @year = year
   end
+  def all
+    #filter from array when type Planet
+  end
 end
 
 class Star < Body
@@ -58,12 +84,16 @@ end
 solar_system = System.new
 earth = Planet.new('Planet Earth', '2929388440', '24', '365')
 solar_system.add(earth)
-puts solar_system.bodies.inspect
-puts "*"*50
+# puts solar_system.bodies.inspect
+# puts "1"*50
 moon = Moon.new('Moon', '1231421242', earth, 1)
 solar_system.add(moon)
-puts solar_system.bodies.inspect
-puts "*"*50
+# puts solar_system.bodies.inspect
+# puts "2"*50
 sun = Star.new('Sun', '234324', 'G-Star')
 solar_system.add(sun)
 puts solar_system.bodies.inspect
+puts "I'm trying to add an existent Celestial Body to the array. It should give me an error."
+sun2 = Star.new('Sun', '234324', 'G-Star')
+solar_system.add(sun2)
+# puts solar_system.bodies.length
