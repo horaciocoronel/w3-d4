3e9b1a8d96e0fa5feb904a780ee873415ec41d1c
horaciocoronel <arielc@gmail.com>
2017-09-28 13:27:43 -0400

    Exercise 2.1.8

--- object_oriented_programming/people.rb
+++ object_oriented_programming/people.rb
@@ -25,3 +25,13 @@ nadia = Instructor.new('Nadia')
 nadia.greeting
 chris = Student.new('Chris')
 chris.greeting
+nadia.teach
+chris.learn
+
+# call the teach method on your student instance. What happens? Why doesn't that work? Leave a comment in your program explaining why.
+chris.teach
+nadia.learn
+# Both methods won't work because:
+# Nadia can not learn, because on the instructor class we didn't define the learn method
+# Chris can not teach, because on the student class we didn't define a teach method.
+# They belong to the class Person, so whatever is inside that class they'd inherit
